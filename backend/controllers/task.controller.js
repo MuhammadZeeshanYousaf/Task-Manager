31c135fde0e7250dab0c4f15910c5666023f95d9
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-09-12 15:00:26 +0500

    Create full CRUD for task.

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -1,10 +1,13 @@
+import mongoose from "mongoose";
 import Task from "../models/task.model.js";
 
 export const createTask = async (req, res) => {
   const taskBody = req.body;
 
   if (!taskBody.title) {
-    res.status(400).json({ ok: false, message: "Task title is required." });
+    return res
+      .status(400)
+      .json({ ok: false, message: "Task title is required." });
   }
 
   const newTask = { title: taskBody.title, description: taskBody.description };
@@ -20,11 +23,58 @@ export const createTask = async (req, res) => {
 
 export const showTask = async (req, res) => {
   const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ ok: false, message: "Invalid task id!" });
 
   try {
     const task = await Task.find({ _id: id });
     res.status(200).json({ ok: true, data: task });
   } catch (e) {
-    res.status(404).json({ ok: true, message: e.message });
+    res.status(404).json({ ok: false, message: e.message });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ ok: false, message: "Invalid task id!" });
+
+  try {
+    const task = await Task.findByIdAndDelete({ _id: id });
+    if (task) res.status(200).json({ ok: true, data: task });
+    else res.status(200).json({ ok: false, message: "Task not found!" });
+  } catch (e) {
+    res.status(500).json({ ok: false, message: e.message });
+  }
+};
+
+export const allTasks = async (_, res) => {
+  try {
+    const task = await Task.find({});
+    res.status(200).json({ ok: true, data: task });
+  } catch (e) {
+    res.status(500).json({ ok: false, message: e.message });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const task = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ ok: false, message: "Invalid task id!" });
+
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      {
+        title: task.title,
+        description: task.description,
+      },
+      { new: true }
+    );
+    res.status(200).json({ ok: true, data: updatedTask });
+  } catch (e) {
+    res.status(500).json({ ok: false, message: e.message });
   }
 };
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { createTask, showTask } from "../controllers/task.controller.js";
+import {
+  createTask,
+  deleteTask,
+  allTasks,
+  showTask,
+  updateTask,
+} from "../controllers/task.controller.js";
 
 const router = express.Router();
 
 router.post("/", createTask);
 router.get("/:id", showTask);
+router.delete("/:id", deleteTask);
+router.get("/", allTasks);
+router.put("/:id", updateTask);
+router.patch("/:id", updateTask);
 
 export default router;
